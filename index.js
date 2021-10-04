ae23b00d0c96752a6e97880089316cb41815435f
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2021-10-04 15:14:15 -0300

    parte dos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Ejercicios con Objetos, JSON y Local Storage
 
+// PARTE 1
+
 // 1. Crea tres objetos usuario1, usuario2, usuario3 que tengan las propiedades nombreUsuario y contrasenia como strings.
 
 
@@ -22,13 +24,13 @@ const usuario3 = {
 
 const body = document.getElementById("body")
 
-const saludar = (objeto) => {
-    return body.innerHTML = `<h1>Hola, ${objeto.nombreUsuario}</h1>`
-}
+// const saludar = (objeto) => {
+//     return body.innerHTML = `<h1>Hola, ${objeto.nombreUsuario}</h1>`
+// }
 
 // 3. Probá tu función con los tres objetos definidos antes.
 
-saludar(usuario1)
+// saludar(usuario1)
 // saludar(usuario2)
 // saludar(usuario3)
 
@@ -40,8 +42,8 @@ const modificarNombreDeUsuario = (objeto, nuevoNombre) => {
 
 // 5.Probá tu función con los tres objetos definidos antes.
 
-console.log(modificarNombreDeUsuario(usuario1, "tatianamodic"))
-console.log(usuario1)
+// console.log(modificarNombreDeUsuario(usuario1, "tatianamodic"))
+// console.log(usuario1)
 
 // 6. Definí una función modificarContrasenia que reciba dos parametros: un objeto usuario y un string nuevaContrasenia. La función debe retornar el objeto con la propiedad contrasenia modificada para tener el valor de nuevaContrasenia.
 
@@ -49,8 +51,8 @@ const modificarContrasenia = (objeto, nuevaContrasenia) => {
     return objeto.contrasenia = nuevaContrasenia
 }
 
-console.log(modificarContrasenia(usuario1, "tata9090"))
-console.log(usuario1)
+// console.log(modificarContrasenia(usuario1, "tata9090"))
+// console.log(usuario1)
 
 // 7. Crea la función convertirAJSON. La función debe recibir un objeto usuario como parámetro y retornar el objeto convertido a JSON.
 
@@ -60,7 +62,7 @@ const convertirAJSON = (objeto) => {
 
 //9.
 let usuario1JSON = convertirAJSON(usuario1)
-console.log(usuario1JSON)
+// console.log(usuario1JSON)
 
 // 8. Crea la función convertirDesdeJSON. La función debe recibir una cadena JSON objetoJSON y retornar la cadena convertida a un objeto de Javascript.
 
@@ -70,7 +72,7 @@ const convertirDesdeJSON = (cadenaJSON) => {
 
 //9.
 let cadenaDesdeJSON = convertirDesdeJSON(usuario1JSON)
-console.log(cadenaDesdeJSON)
+// console.log(cadenaDesdeJSON)
 
 // 11. Definí la función guardarEnLocalStorage que reciba como parámetro un objeto de Javascript y un string, y guarde en localStorage la cadena con el string como nombre de la clave (Recordá que antes de guardar un objeto en localStorage hay que convertirlo a JSON: usá la función convertirAJSON que declaraste antes)
 
@@ -88,4 +90,129 @@ const leerDesdeLocalStorage = (string) => {
     return convertirDesdeJSON(JSONLocalStorage)
 }
 
-console.log(leerDesdeLocalStorage("Usuario 3"))
+// console.log(leerDesdeLocalStorage("Usuario 3"))
+
+
+// PARTE 2 
+
+//USUARIO
+const usuario = {
+    nombreUsuario: "barbarakr",
+    contrasenia: "123hola",
+}
+
+//Elementos
+const sectionBienvenida = document.getElementById("section-bienvenida")
+const sectionFormularioInicio = document.getElementById("section-formulario-inicio")
+const tituloHeader = document.getElementById("titulo-header")
+const sectionFormularioNuevoUsuario = document.getElementById("section-formulario-cambiar-usuario")
+
+
+const botonIniciarSesion = document.getElementById("boton-iniciar-sesion")
+const usuarioIngresado = document.getElementById("input-usuario")
+const contraseniaIngresada = document.getElementById("input-contrasenia")
+
+const botonEnviar = document.getElementById("boton-enviar")
+
+const botonCambiarDatos = document.getElementById("cambiar-datos")
+const botonCerrarSesion = document.getElementById("cerrar-sesion")
+const usuarioNuevoIngresado = document.getElementById("input-usuario-nuevo")
+const contraseniaNuevaIngresada = document.getElementById("input-contrasenia-nueva")
+const botonEnviarNuevoUsuario = document.getElementById("boton-enviar-nuevo-usuario")
+
+//Funciones auxiliares
+
+const saludar = (objeto) => {
+    return tituloHeader.textContent = `Hola ${objeto.nombreUsuario}`
+}
+if(leerDesdeLocalStorage("usuario") === true) {
+    saludar(usuario)
+        botonEnviar.classList.add("is-hidden");
+        botonCambiarDatos.classList.remove("is-hidden");
+        botonCerrarSesion.classList.remove("is-hidden");
+}
+
+
+
+
+botonEnviar.addEventListener("click", function(event){
+    event.preventDefault()
+  })
+botonCambiarDatos.addEventListener("click", function(event){
+    event.preventDefault()
+  })
+  botonCerrarSesion.addEventListener("click", function(event){
+    event.preventDefault()
+  })
+botonEnviarNuevoUsuario.addEventListener("click", function(event){
+    event.preventDefault()
+  })
+  
+
+botonIniciarSesion.onclick = () => {
+    sectionFormularioInicio.classList.toggle("is-hidden") 
+}
+
+
+
+let sesionIniciada = false 
+
+const revisarDatos = (usuario) => {
+    let sesionIniciada = false
+    if(usuarioIngresado.value === usuario.nombreUsuario && contraseniaIngresada.value === contraseniaIngresada) {
+        sesionIniciada = true;
+    }
+    return sesionIniciada
+}
+
+botonEnviar.onclick = () => {
+    revisarDatos(usuario);
+    if (sesionIniciada = true) {
+        saludar(usuario)
+        botonEnviar.classList.add("is-hidden");
+        botonCambiarDatos.classList.remove("is-hidden");
+        botonCerrarSesion.classList.remove("is-hidden");
+        usuario.sesionIniciada = true;
+        console.log(usuario)
+        guardarEnLocalStorage(usuario.sesionIniciada, "usuario")
+    }
+    
+}
+
+botonCerrarSesion.onclick = () => {
+    tituloHeader.textContent = "Hola"
+    botonEnviar.classList.remove("is-hidden");
+    botonCambiarDatos.classList.add("is-hidden")
+    botonCerrarSesion.classList.add("is-hidden")
+    usuario.sesionIniciada = false
+    guardarEnLocalStorage(usuario.sesionIniciada, "usuario")
+}
+
+botonCambiarDatos.onclick = () => {
+    sectionFormularioInicio.classList.add("is-hidden")
+    sectionFormularioNuevoUsuario.classList.remove("is-hidden")
+    
+}
+
+
+botonEnviarNuevoUsuario.onclick = () => {
+    modificarNombreDeUsuario(usuario, usuarioNuevoIngresado.value)
+    modificarContrasenia(usuario, contraseniaNuevaIngresada.value)
+    sectionFormularioInicio.classList.remove("is-hidden")
+    sectionFormularioNuevoUsuario.classList.add("is-hidden")
+    guardarEnLocalStorage(usuario.sesionIniciada, "usuario")
+    saludar(usuario)
+    
+
+    console.log(usuario)
+}
+
+
+
+
+
+
+
+
+
+
